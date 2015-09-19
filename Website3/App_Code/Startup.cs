58b4e8fe4c225b886c45fde80ad579cc904f124c
kgibson5 <kgibson5@WA322-16.nait.ca>
2015-09-18 19:25:06 -0600

    added website

---
+++ Website3/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website3.Startup))]
+namespace Website3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
